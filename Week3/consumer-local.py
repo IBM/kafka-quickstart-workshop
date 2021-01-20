53ff383f461490a00dbc0a4cab5a7f41a2a3d51b
ekaratnida <69342162+ekaratnida@users.noreply.github.com>
2021-01-20 12:32:07 +0700

    v2 plotter

--- Week3/consumer-local.py
+++ Week3/consumer-local.py
@@ -8,8 +8,12 @@ c = Consumer({
 
 c.subscribe(['streams-wordcount-output'])
 
-import base64
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+figure(num=None, figsize=(18, 6), dpi=80, facecolor='w', edgecolor='k')
+plt.ion()
 
+lr = {}
 while True:
     msg = c.poll(1.0)
 
@@ -20,7 +24,26 @@ while True:
         continue
 
     #print(msg.value())
-    print('Received message: {0} , {1}'.format(msg.key().decode('utf-8'), msg.value()[-1]))
+    value = msg.value()[-1]
+    if value > 12: continue
+    kvalue = msg.key().decode('utf-8')
+    print('Received message: {0} , {1}'.format(kvalue, value))
+    
+    #x = range(value)
+    #y = range(value)
+    # plt.gca().cla() # optionally clear axes
+    lr[kvalue] = value    
+    plt.scatter(list(lr.keys()),list(lr.values()))
+    i = 0
+    for x in range(0,len(list(lr.keys()))): 
+        plt.annotate(list(lr.keys())[x], (i, list(lr.values())[x]+0.2), rotation=90)
+        i = i+1
+    
+    #plt.title("test")
+    #plt.xticks(rotation=90)
+    plt.draw()
+    plt.pause(0.5)
 
+plt.show(block=True)
 c.close()
 
