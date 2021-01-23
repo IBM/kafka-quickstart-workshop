7c1a9271c42b016357d95dc58f5200d3db1264bc
pok <pok@DESKTOP-0JU620D>
2021-01-23 12:10:20 +0700

    Fixed python bug (cannot handle Nonetype

--- Week3/consumer-local.py
+++ Week3/consumer-local.py
@@ -8,10 +8,12 @@ c = Consumer({
 
 c.subscribe(['streams-wordcount-output'])
 
+'''
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure
 figure(num=None, figsize=(18, 6), dpi=80, facecolor='w', edgecolor='k')
 plt.ion()
+'''
 
 lr = {}
 while True:
@@ -24,26 +26,31 @@ while True:
         continue
 
     #print(msg.value())
-    value = msg.value()[-1]
-    if value > 12: continue
+    value = msg.value()
+    print(value)
+    if value is None:
+        value = -1
+    else:
+        value = msg.value()[-1]
+        
     kvalue = msg.key().decode('utf-8')
     print('Received message: {0} , {1}'.format(kvalue, value))
     
     #x = range(value)
     #y = range(value)
     # plt.gca().cla() # optionally clear axes
-    lr[kvalue] = value    
+    '''lr[kvalue] = value    
     plt.scatter(list(lr.keys()),list(lr.values()))
     i = 0
     for x in range(0,len(list(lr.keys()))): 
         plt.annotate(list(lr.keys())[x], (i, list(lr.values())[x]+0.2), rotation=90)
         i = i+1
-    
+    '''
     #plt.title("test")
     #plt.xticks(rotation=90)
-    plt.draw()
-    plt.pause(0.5)
+    '''plt.draw()
+    plt.pause(0.5)'''
 
-plt.show(block=True)
+'''plt.show(block=True)'''
 c.close()
 
